547dc5ce550fc6ee5bdf0c02db4a9a80c77482f0
Hastings Greer <hastings@hgreer.com>
2022-01-13 19:48:51 -0500

    move matrix function to glutils since we won't edit it

--- glutils.js
+++ glutils.js
@@ -1,4 +1,5 @@
 
+import {mat4} from 'gl-matrix'
 function getCursorPos(canvas, event){
     const rect = canvas.getBoundingClientRect();
     return [event.clientX - rect.left, event.clientY - rect.top];
@@ -37,5 +38,38 @@ function loadShader(gl, type, source) {
 
   return shader;
 }
+function createMatrices(gl){
+  // Create a perspective matrix, a special matrix that is
+  // used to simulate the distortion of perspective in a camera.
+  // Our field of view is 45 degrees, with a width/height
+  // ratio that matches the display size of the canvas
+  // and we only want to see objects between 0.1 units
+  // and 100 units away from the camera.
 
-export {getCursorPos, initShaderProgram, loadShader}
+  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
+
+  // note: glmatrix.js always has the first argument
+  // as the destination to receive the result.
+  mat4.perspective(projectionMatrix,
+                   fieldOfView,
+                   aspect,
+                   zNear,
+                   zFar);
+
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
+
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+                 modelViewMatrix,     // matrix to translate
+                 [-0.0, 0.0, -2.5]);  // amount to translate
+  return [projectionMatrix, modelViewMatrix];
+}
+export {getCursorPos, initShaderProgram, loadShader, createMatrices}
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import BigNumber from 'bignumber.js'
-import {mat4} from 'gl-matrix'
-import {getCursorPos, initShaderProgram, loadShader} from './glutils.js'
+import {getCursorPos, initShaderProgram, loadShader, createMatrices} from './glutils.js'
 let mandelbrot_state = {
     center: [0, 0],
     radius: 2,
@@ -52,7 +51,7 @@ function main() {
     }
 
 
-// Vertex shader program
+  // Vertex shader program
 
   const vsSource = `#version 300 es
     in vec4 aVertexPosition;
@@ -170,39 +169,8 @@ function drawScene(gl, programInfo, buffers) {
   // Clear the canvas before we start drawing on it.
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Create a perspective matrix, a special matrix that is
-  // used to simulate the distortion of perspective in a camera.
-  // Our field of view is 45 degrees, with a width/height
-  // ratio that matches the display size of the canvas
-  // and we only want to see objects between 0.1 units
-  // and 100 units away from the camera.
-
-  const fieldOfView = 45 * Math.PI / 180;   // in radians
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-
-  // note: glmatrix.js always has the first argument
-  // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
-
-  // Set the drawing position to the "identity" point, which is
-  // the center of the scene.
-  const modelViewMatrix = mat4.create();
-
-  // Now move the drawing position a bit to where we want to
-  // start drawing the square.
-
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -2.5]);  // amount to translate
-
+  let projectionMatrix, modelViewMatrix;
+  [projectionMatrix, modelViewMatrix] = createMatrices(gl)
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   {
