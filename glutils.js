e3f77a9c2e5d07d7927d7997cb60fa126308fde6
Hastings Greer <hastings.greer@gmail.com>
2023-07-14 20:50:40 -0700

    cleanup on aisle 4

--- glutils.js
+++ glutils.js
@@ -1,16 +1,12 @@
-
-import {mat4} from 'gl-matrix'
-function getCursorPos(canvas, event){
-    const rect = canvas.getBoundingClientRect();
-    return [event.clientX - rect.left, event.clientY - rect.top];
+import { mat4 } from "gl-matrix";
+function getCursorPos(canvas, event) {
+  const rect = canvas.getBoundingClientRect();
+  return [event.clientX - rect.left, event.clientY - rect.top];
 }
 
 function getTouchPos(canvas, touchEvent) {
   var rect = canvasDom.getBoundingClientRect();
-  return [
-    touchEvent.touches[0].clientX - rect.left,
-    touchEvent.touches[0].clientY - rect.top
-  ];
+  return [touchEvent.touches[0].clientX - rect.left, touchEvent.touches[0].clientY - rect.top];
 }
 
 function initShaderProgram(gl, vsSource, fsSource) {
@@ -23,7 +19,7 @@ function initShaderProgram(gl, vsSource, fsSource) {
   gl.linkProgram(shaderProgram);
 
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
     return null;
   }
 
@@ -38,14 +34,14 @@ function loadShader(gl, type, source) {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return null;
   }
 
   return shader;
 }
-function createMatrices(gl){
+function createMatrices(gl) {
   // Create a perspective matrix, a special matrix that is
   // used to simulate the distortion of perspective in a camera.
   // Our field of view is 45 degrees, with a width/height
@@ -53,7 +49,7 @@ function createMatrices(gl){
   // and we only want to see objects between 0.1 units
   // and 100 units away from the camera.
 
-  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const fieldOfView = (45 * Math.PI) / 180; // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
@@ -61,11 +57,7 @@ function createMatrices(gl){
 
   // note: glmatrix.js always has the first argument
   // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
   // Set the drawing position to the "identity" point, which is
   // the center of the scene.
@@ -74,9 +66,11 @@ function createMatrices(gl){
   // Now move the drawing position a bit to where we want to
   // start drawing the square.
 
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -2.5]);  // amount to translate
+  mat4.translate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to translate
+    [-0.0, 0.0, -2.5],
+  ); // amount to translate
   return [projectionMatrix, modelViewMatrix];
 }
 
@@ -96,29 +90,36 @@ function loadTexture(gl, url) {
   const border = 0;
   const srcFormat = gl.RGBA;
   const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                width, height, border, srcFormat, srcType,
-                pixel);
+  const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    width,
+    height,
+    border,
+    srcFormat,
+    srcType,
+    pixel,
+  );
 
   const image = new Image();
-  image.onload = function() {
+  image.onload = function () {
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                  srcFormat, srcType, image);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
 
     // WebGL1 has different requirements for power of 2 images
     // vs non power of 2 images so check if the image is a
     // power of 2 in both dimensions.
     if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-       // Yes, it's a power of 2. Generate mips.
-       gl.generateMipmap(gl.TEXTURE_2D);
+      // Yes, it's a power of 2. Generate mips.
+      gl.generateMipmap(gl.TEXTURE_2D);
     } else {
-       // No, it's not a power of 2. Turn off mips and set
-       // wrapping to clamp to edge
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      // No, it's not a power of 2. Turn off mips and set
+      // wrapping to clamp to edge
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     }
   };
   image.src = url;
@@ -130,5 +131,4 @@ function isPowerOf2(value) {
   return (value & (value - 1)) == 0;
 }
 
-
-export {getCursorPos, getTouchPos, initShaderProgram, loadShader, createMatrices, loadTexture}
+export { getCursorPos, getTouchPos, initShaderProgram, loadShader, createMatrices, loadTexture };
--- main.js
+++ main.js
@@ -1,87 +1,75 @@
-import './style.css'
-import BigNumber from 'bignumber.js'
-import {getCursorPos, getTouchPos, initShaderProgram, loadShader, createMatrices, loadTexture} from './glutils.js'
+import "./style.css";
+import BigNumber from "bignumber.js";
+import {
+  getCursorPos,
+  getTouchPos,
+  initShaderProgram,
+  loadShader,
+  createMatrices,
+  loadTexture,
+} from "./glutils.js";
 let mandelbrot_state = {
-    center: [0, 0],
-    radius: 2,
-    callbacks: [],
-    modified: function() {
-        for (const cb of this.callbacks){
-            cb()
-        }
-    },
-    set: function(x, y, r) {
-        this.center = [x, y];
-        this.radius = r;
-        this.modified()
-    },
-    update: function(dx, dy) {
-        this.center = [this.center[0] + this.radius * dx, this.center[1] - this.radius * dy]
-        this.radius = this.radius / 2;
-        this.modified()
+  center: [0, 0],
+  radius: 2,
+  callbacks: [],
+  modified: function () {
+    for (const cb of this.callbacks) {
+      cb();
     }
-}
-
-main()
-
+  },
+  set: function (x, y, r) {
+    this.center = [x, y];
+    this.radius = r;
+    this.modified();
+  },
+  update: function (dx, dy) {
+    this.center = [this.center[0] + this.radius * dx, this.center[1] - this.radius * dy];
+    this.radius = this.radius / 2;
+    this.modified();
+  },
+};
+main();
 function main() {
-
-    document.querySelector("#reset").addEventListener('click', (event) => {
-        mandelbrot_state.set(0, 0, 2)
-    });
-
-    const canvas = document.querySelector('#canvas')
-
-    canvas.addEventListener('click', (event) => {
-        let x, y
-        [x, y] = getCursorPos(canvas, event);
-        x = x / 350 - 1;
-        y = y / 350 - 1;
-        mandelbrot_state.update(x, y)
-    });
-    canvas.addEventListener("touchstart", (event) => {
-        let x, y
-        [x, y] = getTouchPos(canvas, event);
-        x = x / 350 - 1;
-        y = y / 350 - 1;
-        mandelbrot_state.update(x, y)
-    });
-    mandelbrot_state.callbacks.push(()=>{
-
-        document.querySelector("#clickpos").innerText = mandelbrot_state.center
-    });
-    const gl = canvas.getContext('webgl2')
-
-    if (!gl) {
-        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
-        return;
-    }
-
-
-  // Vertex shader program
-
+  document.querySelector("#reset").addEventListener("click", (event) => {
+    mandelbrot_state.set(0, 0, 2);
+  });
+  const canvas = document.querySelector("#canvas");
+  canvas.addEventListener("click", (event) => {
+    let x, y;
+    [x, y] = getCursorPos(canvas, event);
+    x = x / 350 - 1;
+    y = y / 350 - 1;
+    mandelbrot_state.update(x, y);
+  });
+  canvas.addEventListener("touchstart", (event) => {
+    let x, y;
+    [x, y] = getTouchPos(canvas, event);
+    x = x / 350 - 1;
+    y = y / 350 - 1;
+    mandelbrot_state.update(x, y);
+  });
+  mandelbrot_state.callbacks.push(() => {
+    document.querySelector("#clickpos").innerText = mandelbrot_state.center;
+  });
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
   const vsSource = `#version 300 es
     in vec4 aVertexPosition;
-	
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
-
 	out highp vec2 delta;
-	
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 	  delta = vec2(aVertexPosition[0], aVertexPosition[1]);
     }
   `;
-
-  // Fragment shader program
   const fsSource = `#version 300 es
     precision highp float;
     in highp vec2 delta;
-
     out vec4 fragColor;
-	
-
     uniform vec4 uState;
     uniform sampler2D sequence;
     float get_orbit_x(int i) {
@@ -99,7 +87,6 @@ function main() {
      float dcy = uState[2] * delta[1];
      float x;
      float y;
-
      float dx = 0.;
      float dy = 0.;
      int j = 0;
@@ -112,13 +99,10 @@ function main() {
 	float tx = 2. * x * dx - 2. * y * dy + dx * dx - dy * dy + dcx;
 	dy = 2. * x * dy + 2. * y * dx + 2. * dx * dy + dcy;
 	dx = tx;
-
 	x = get_orbit_x(k);
 	y = get_orbit_y(k);
-
 	float fx = x + dx;
 	float fy = y + dy;
-
 	if (fx * fx + fy * fy > 4.){
 	break;
 	}
@@ -134,112 +118,72 @@ function main() {
       fragColor = vec4(vec3(cos(c), cos(1.1214 * c) , cos(.8 * c)) / -2. + .5, 1.);
     }
   `;
-
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-
-  // Collect all the info needed to use the shader program.
-  // Look up which attribute our shader program is using
-  // for aVertexPosition and look up uniform locations.
   const programInfo = {
     program: shaderProgram,
     attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
     },
     uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-      state: gl.getUniformLocation(shaderProgram, "uState")
+      projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+      state: gl.getUniformLocation(shaderProgram, "uState"),
     },
   };
-
   const buffers = initBuffers(gl);
   const tex = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, tex);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-
-  // Draw the scene
-  mandelbrot_state.callbacks.push( () => {
+  mandelbrot_state.callbacks.push(() => {
     drawScene(gl, programInfo, buffers);
   });
   mandelbrot_state.modified();
 }
-
 function initBuffers(gl) {
-
-
   const positionBuffer = gl.createBuffer();
-
-  // Select the positionBuffer as the one to apply buffer
-  // operations to from here out.
-
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Now create an array of positions for the square.
-
-  const positions = [
-     1.0,  1.0,
-    -1.0,  1.0,
-     1.0, -1.0,
-    -1.0, -1.0,
-  ];
-
-  // Now pass the list of positions into WebGL to build the
-  // shape. We do this by creating a Float32Array from the
-  // JavaScript array, then use it to fill the current buffer.
-
-  gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(positions),
-                gl.STATIC_DRAW);
-
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
   return {
     position: positionBuffer,
   };
 }
-function make_reference_orbit(){
-	var cx = mandelbrot_state.center[0];
-	var cy = mandelbrot_state.center[1];
-	var x = 0;
-	var y = 0;
-	var j = 0;
-	var orbit = new Float32Array(20000);
-	for(var i = 0; i < 20000; i++){
-		orbit[i] = -1;
-	}
-	for(var i = 0; i < 10000; i ++){
-		orbit[2 * i] = x;
-		orbit[2 * i + 1] = y;
-		j += 1;
-		var tx = x * x - y * y + cx;
-		y = 2. * x * y + cy;
-		x = tx;
-		if (x * x + y * y > 400) {
-			break;
-		}
-	}
-	return orbit;
+function make_reference_orbit() {
+  var cx = mandelbrot_state.center[0];
+  var cy = mandelbrot_state.center[1];
+  var x = 0;
+  var y = 0;
+  var j = 0;
+  var orbit = new Float32Array(20000);
+  for (var i = 0; i < 20000; i++) {
+    orbit[i] = -1;
+  }
+  for (var i = 0; i < 10000; i++) {
+    orbit[2 * i] = x;
+    orbit[2 * i + 1] = y;
+    j += 1;
+    var tx = x * x - y * y + cx;
+    y = 2 * x * y + cy;
+    x = tx;
+    if (x * x + y * y > 400) {
+      break;
+    }
+  }
+  return orbit;
 }
-
-//
-// Draw the scene.
-//
 function drawScene(gl, programInfo, buffers) {
   var orbit = make_reference_orbit();
   var values = new Float32Array(orbit);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.R32F, 100, 200, 0, gl.RED, gl.FLOAT, values);
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-  // Clear the canvas before we start drawing on it.
-
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+  gl.clearDepth(1.0); // Clear everything
+  gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   let projectionMatrix, modelViewMatrix;
-  [projectionMatrix, modelViewMatrix] = createMatrices(gl)
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
+  [projectionMatrix, modelViewMatrix] = createMatrices(gl);
   {
     const numComponents = 2;
     const type = gl.FLOAT;
@@ -248,38 +192,28 @@ function drawScene(gl, programInfo, buffers) {
     const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset,
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
-  
-	
   gl.useProgram(programInfo.program);
-
-  // Set the shader uniforms
-
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
+  gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
+  gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
   gl.uniform4f(
-      programInfo.uniformLocations.state,
-      mandelbrot_state.center[0], mandelbrot_state.center[1], mandelbrot_state.radius, 0);
-
+    programInfo.uniformLocations.state,
+    mandelbrot_state.center[0],
+    mandelbrot_state.center[1],
+    mandelbrot_state.radius,
+    0,
+  );
   {
     const offset = 0;
     const vertexCount = 4;
     gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
   }
 }
-
-
